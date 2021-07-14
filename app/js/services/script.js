cd25fee8357d131f6f15a835f7a4d01e7b572ca6
wxweb@mail.ru <wxweb@mail.ru>
2021-07-14 13:25:18 +0300

    Верстка двух страниц с адаптивом: Главная страница и страница услуг.

---
+++ app/js/services/script.js
@@ -0,0 +1,34 @@
+$('.header__burger').on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('.header__nav').toggleClass('active');
+});
+
+$('.faq__question').on('click', function() {
+    $(this).parent('li').toggleClass('active');
+});
+
+$('.js-callback-toggle').on('click', function(e) {
+    e.preventDefault();
+    $('.popup-callback').toggleClass('active');
+    $('body').addClass('no-scroll');
+});
+
+$('.popup__form-close').on('click', function(e) {
+    e.preventDefault();
+    $(this).parent('.form').parent('.popup').removeClass('active');
+    $('body').removeClass('no-scroll');
+});
+
+$(document).click(function(e) {
+    if ($(e.target).closest('.popup__inner').length || $(e.target).closest('.js-callback-toggle').length) {
+        return;
+    }
+    $('.popup').removeClass('active');
+});
+
+$("a[href^='#scroll']").click(function() {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserSync = require('browser-sync');
+let concat = require('gulp-concat');
+let rename = require('gulp-rename');
+let autoprefixer = require('gulp-autoprefixer');
+let uglify = require('gulp-uglify-es').default;
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+        .pipe(browserSync.reload({ stream: true }))
+})
+
+gulp.task('script', function() {
+    return gulp.src('app/js/*.js')
+        .pipe(browserSync.reload({ stream: true }))
+})
+
+gulp.task('scss', function() {
+    return gulp.src('app/scss/index/style.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(autoprefixer('last 5 versions'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('app/css/index'))
+        .pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "app",
+            index: 'services.html'
+        }
+    });
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'))
+    gulp.watch('app/**/*.html', gulp.parallel('html'))
+    gulp.watch('app/js/*.js', gulp.parallel('script'))
+});
+
+gulp.task('default', gulp.parallel('scss', 'browserSync', 'watch'));
